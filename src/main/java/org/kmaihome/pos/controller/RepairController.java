d024db7acdfb1fdce16d0dedf001f28305b4e71b
AslinMA <asankadilshan198@gmail.com>
2026-01-08 23:10:48 +0530

    Add repair tracking system with inventory management and discount support

--- src/main/java/org/kmaihome/pos/controller/RepairController.java
+++ src/main/java/org/kmaihome/pos/controller/RepairController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/repair")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "false")
+@CrossOrigin(origins = "*", allowCredentials = "false")
 public class RepairController {
 
     @Autowired
